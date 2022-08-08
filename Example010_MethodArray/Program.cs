2bb7a4bb005ede8f49118857527f953423c228e5
Lubov <pitisyka@yandex.ru>
2022-08-08 16:01:37 +0500

    add example 10

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿// есть одномерный массив array из n эл-ов, найти элемент массива равный find 
+
+int [] array  = {1, 58, 7, 54, 88, 2, 6, 71};
+int n = array.Length;
+int find = 2;
+for (int index = 0; index < n; index ++)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+}
+
